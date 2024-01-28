cf4b3413d5771ec5519bd77c1153ab84eb273b03
ElMALIHI <118868470+ElMALIHI@users.noreply.github.com>
2024-01-28 09:24:07 +0100

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -14,21 +14,6 @@ export function SiteHeader() {
         <div className="flex items-center justify-end flex-1 space-x-4">
           <nav className="flex items-center space-x-1">
             <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.gitHub className="w-5 h-5" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            <Link
               href={siteConfig.links.twitter}
               target="_blank"
               rel="noreferrer"
